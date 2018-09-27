f3316ef68d992334b5ac6cd7d42322debe855b63
Luke Robinson <luke.robinson@blackcatsolutions.co.uk>
2018-09-27 11:42:00 +0100

    Initial commit, webpack config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Phaser from 'phaser'
+
+const preload = () => { }
+const create = () => { }
+const update = () => { }
+
+const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
+  preload: preload, create: create, update: update
+});
