69c90d5eb5bcb8190184e1359a8143a174b7cc1c
Luke Robinson <luke.robinson@blackcatsolutions.co.uk>
2018-09-28 09:09:41 +0100

    Render the paddle and ball, registered input events

--- src/const/index.js
+++ src/const/index.js
@@ -1,8 +1,14 @@
+export const assets = {
+    bricks: ['blue1', 'red1', 'green1', 'yellow1', 'silver1', 'purple1'],
+    ball: 'ball1',
+    paddle: 'paddle1'
+}
+
+export const keys = {
+    assets: 'assets'
+}
+
 export default {
-    blue1: 'blue1',
-    red1: 'red1',
-    green1: 'green1',
-    yellow1: 'yellow1',
-    silver1: 'silver1',
-    purple1: 'purple1'
+    assets,
+    keys
 }
--- src/scenes/Breakout.js
+++ src/scenes/Breakout.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser'
-import assets from '../const'
+import { assets, keys } from '../const'
 
 class Breakout extends Phaser.Scene {
     constructor() {
@@ -13,30 +13,61 @@ class Breakout extends Phaser.Scene {
     }
 
     preload() {
-        console.log('preloading')
-        this.load.atlas('assets', 'assets/breakout.png', 'assets/breakout.json')
+        this.load.atlas(keys.assets, 'assets/breakout.png', 'assets/breakout.json')
     }
 
     create() {
-        console.log('creating')
-
         this.physics.world.setBoundsCollision(true, true, true, false)
 
-        const frame = [
-            assets.blue1,
-            assets.red1,
-            assets.green1,
-            assets.yellow1,
-            assets.silver1,
-            assets.purple1
-        ]
+        this.setUpObjects()
 
+        this.physics.add.collider(this.ball, this.bricks, this.hitBrick, null, this)
+        this.physics.add.collider(this.ball, this.paddle, this.hitPaddle, null, this)
+    }
+
+    setUpObjects() {
         this.bricks = this.physics.add.staticGroup({
-            key: 'assets',
-            frame,
+            key: keys.assets,
+            frame: assets.bricks,
             frameQuantity: 10,
             gridAlign: { width: 10, height: 6, cellWidth: 64, cellHeight: 32, x: 112, y: 100 }
         })
+
+        this.ball = this.physics.add
+            .image(400, 500, keys.assets, assets.ball)
+            .setCollideWorldBounds(true)
+            .setBounce(1)
+
+        this.ball.setData('onPaddle', true)
+
+        this.paddle = this.physics.add.image(400, 550, keys.assets, assets.paddle).setImmovable()
+
+        this.setUpInputEvents()
+    }
+
+    setUpInputEvents() {
+        this.input.on(
+            'pointermove',
+            pointer => {
+                this.paddle.x = Phaser.Math.Clamp(pointer.x, 52, 748)
+
+                if (this.ball.getData('onPaddle')) {
+                    this.ball.x = this.paddle.x
+                }
+            },
+            this
+        )
+
+        this.input.on(
+            'pointerup',
+            pointer => {
+                if (this.ball.getData('onPaddle')) {
+                    this.ball.setVelocity(-75, -300)
+                    this.ball.setData('onPaddle', false)
+                }
+            },
+            this
+        )
     }
 }
 
